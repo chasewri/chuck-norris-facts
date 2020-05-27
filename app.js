1bfc02d8d005b75ed6d3412d6f615727373f7063
chasewri <chasewri@gmail.com>
2020-05-26 18:07:33 -0600

    package update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.request(helmet());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
