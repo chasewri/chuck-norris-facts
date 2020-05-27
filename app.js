7ecdcd4e39478d3d64a70c78954fb7a771274207
chasewri <chasewri@gmail.com>
2020-05-26 18:24:52 -0600

    fix app js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.request(helmet());
+app.use(helmet());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
